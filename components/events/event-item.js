d1ad25d24ab591ee6362c17793ec9086f9a198c9
Alex Gochenour <alexgochenour@gmail.com>
2022-07-01 17:37:11 -0400

    feat: more icons

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,8 @@
-import Link from 'next/link';
 import classes from './event-item.module.css';
 import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 export default function EventItem(props){
   const { title, image, date, location, id } = props;
@@ -16,19 +18,24 @@ export default function EventItem(props){
     <li className={classes.item}>
       <img src={'/' + image} alt='Preview image of the event.' />
       <div className={classes.content}>
-          <div className={classes.summary}>
+          <section>
             <h2>{title}></h2>
             <div className={classes.date}>
+              <DateIcon></DateIcon>
               <time>{humanReadableDate}</time>
             </div>
             <div className={classes.address}>
+              <AddressIcon></AddressIcon>
               <address>{formattedAddress}</address>
             </div>
-          </div>
+          </section>
           <div className={classes.actions}>
-            <Button link={exploreLink}>Explore Event</Button>
+            <Button link={exploreLink}>
+              <span>Explore Event</span>
+              <span className={classes.icon}><ArrowRightIcon/></span>
+            </Button>
           </div>
       </div>
     </li>
-  )
+  );
 }
