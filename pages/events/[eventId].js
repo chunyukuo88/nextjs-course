411989f04fca0c2f64b1195867ec9cedcfce3a6b
Alex Gochenour <alexgochenour@gmail.com>
2022-07-04 15:51:05 -0400

    feat: Error Alert.

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import Button from '../../components/ui/button';
+import ErrorAlert from '../../components/ui/error-alert';
 
 export default function SpecificEvent(){
   const router = useRouter();
@@ -33,7 +34,7 @@ const Event = (event) => (
 
 const NoEventsFound = () => (
   <>
-    <p>No event found!</p>
+    <ErrorAlert>No event found for this particular query.</ErrorAlert>
     <div className='center'>
       <Button link='/events' >Back to All Events</Button>
     </div>
