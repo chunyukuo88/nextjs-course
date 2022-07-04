9bb8e5ac7e7cb1b41d719c798488b05ebe1aabb4
Alex Gochenour <alexgochenour@gmail.com>
2022-07-04 13:25:41 -0400

    fix: Updated dummy data

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -11,7 +11,6 @@ export default function SpecificEvent(){
     : WithoutFilterData();
 }
 
-
 const WithoutFilterData = () => (
   <div className='center'>Loading...</div>
 );
@@ -19,10 +18,25 @@ const WithoutFilterData = () => (
 const WithFilterData = (filterData) => {
   const [filteredYear, filteredMonth] = [filterData[0], filterData[1]];
 
-  return (
-    <div>
-      <h1>Events for {monthStrings[+filteredMonth]} {filteredYear}:</h1>
-      <div>Here they be:</div>
-    </div>
-  );
-}
+  return (queryParamsNotValidMonthAndYear(filteredMonth, filteredYear))
+    ? InvalidUrlResponse()
+    : EventsForTheMonth(filteredMonth, filteredYear);
+};
+
+const queryParamsNotValidMonthAndYear = (month, year) => {
+  const currentYear = new Date().getFullYear();
+  const queryParamsAreNotNumbers = (isNaN(month) || isNaN(year));
+  const monthOrYearOutOfRange = (month < 1 || month > 12 || year < currentYear || year > currentYear + 1);
+  return (queryParamsAreNotNumbers || monthOrYearOutOfRange);
+};
+
+const InvalidUrlResponse = () => (
+  <h1>Invalid URL. Check URL query parameters</h1>
+);
+
+const EventsForTheMonth = (filteredMonth, filteredYear) => (
+  <div>
+    <h1>Events for {monthStrings[+filteredMonth]} {filteredYear}:</h1>
+    <div>Here they be:</div>
+  </div>
+);
